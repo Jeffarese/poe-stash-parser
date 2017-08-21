9284ccb9d7ac2773a3bc43acfc2ce9949c938126
Jeffarese <jeff@wallapop.com>
2017-08-21 16:49:26 +0200

    Change API url back

--- src/app/core/parser/parser.service.ts
+++ src/app/core/parser/parser.service.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 @Injectable()
 export class ParserService {
   
-  private BASE_URL = 'api/public-stash-tabs';
+  private BASE_URL = 'http://www.pathofexile.com/api/public-stash-tabs';
   
   constructor(private http: HttpClient) {
     this.http.get(this.BASE_URL).subscribe((r: any) => {
